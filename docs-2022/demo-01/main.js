63dbc681f425c91f1a3e12d4de2d313a5823b0f8
Tinywan <756684177@qq.com>
2021-11-14 19:15:58 +0800

    如何使用麦克风

--- docs-2022/demo-01/main.js
+++ docs-2022/demo-01/main.js
@@ -1,4 +1,38 @@
-(async function () {
-    let devices = await navigator.mediaDevices.enumerateDevices();
-    console.log(devices);
-})();
+const App = {
+    data() {
+        return {
+            audioInputDevices: [],
+            selectedAudioDeviceIndex: 0
+        }
+    },
+    mounted() {
+        this._initDevice();
+    },
+    methods: {
+        async _initDevice() {
+            // 读取所有设备
+            let devices = await navigator.mediaDevices.enumerateDevices();
+            console.log(devices);
+            // 筛选输入设备
+            let audioInputDeviceInfo = devices.filter(value => value.kind === 'audioinput')
+            this.audioInputDevices.length = 0;
+            this.audioInputDevices.push(...audioInputDeviceInfo);
+            this.showSelectedDevice();
+        },
+        async showSelectedDevice(){
+            let deviceInfo = this.audioInputDevices[this.selectedAudioDeviceIndex];
+            let constraints = {
+                video: false,
+                audio: true
+            };
+            let stream = await navigator.mediaDevices.getUserMedia(constraints);
+            this.$refs.audio.srcObject = stream;
+        }
+    },
+    watch: {
+        selectedAudioDeviceIndex(){
+            this.showSelectedDevice()
+        }
+    }
+};
+var vm = Vue.createApp(App).mount('#app');
