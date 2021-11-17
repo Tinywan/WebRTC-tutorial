0a5f1207aafb03a405c1a85a49b2862fa5078036
Tinywan <756684177@qq.com>
2021-11-17 11:14:49 +0800

    屏幕共享

--- docs-2022/demo-07/main.js
+++ docs-2022/demo-07/main.js
@@ -9,15 +9,27 @@ const App = {
     },
     methods: {
         async _initDevice (){
-            // (1) 获取课件视频
-            this._documentStream = await navigator.mediaDevices.getDisplayMedia();
-            // 预览
-            this.$refs.document.srcObject = this._documentStream;
+            // (1) 获取摄像头的音频和视频
+            let cameraConstraints = {
+                video:true,
+                audio:true
+            };
+            this._cameraStream = await navigator.mediaDevices.getUserMedia(cameraConstraints);
+            this.$refs.videoPreview.srcObject = this._cameraStream;
 
-            // (2) 获取摄像头的音频和视频
-            this._cameraStream = await navigator.mediaDevices.getUserMedia({video:true,audio:true});
+            // (2) 获取课件视频
+            let displayMediaOptions = {
+                video: {
+                    cursor: "always"
+                },
+                audio: false
+            }
+            this._documentStream = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
             // 预览
-            this.$refs.preview.srcObject = this._cameraStream;
+            this.$refs.documentPreview.srcObject = this._documentStream;
+
+            // this._context2d = this.$refs.documentPreview.getContext("2d");
+            // this._context2d.drawImage(this._documentStream,0,0,400,300)
 
             // (3) 合并音轨
             // this._cameraStream.getTracks().forEach(value => this._documentStream.addTrack(value));
